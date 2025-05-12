a7b05393c7adfa7fc4d5cf4066ceec85c07951f4
bastien <bastien.raoult1pro@gmail.com>
2025-05-12 16:38:44 +0200

    ✨(NavBar) Add nav-bar + init project [#1]

---
+++ src/components/organisms/Navbar.jsx
@@ -0,0 +1,22 @@
+import NavItem from '../molecules/NavItem';
+// import UserGreeting from '../atoms/UserGreeting'; // ← décommente si nécessaire
+import { faThLarge, faGraduationCap, faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+
+  return (
+    <header className="navbar">
+      <ul className="nav-menu">
+        <NavItem icon={faThLarge} label="Home" href="home" />
+        <NavItem icon={faGraduationCap} label="My learning" href="home" />
+        <NavItem icon={faUser} label="My account" href="home" />
+      </ul>
+
+      {/* <div className="navbar__right">
+        <UserGreeting name="Bastien" />
+      </div> */}
+    </header>
+  );
+};
+
+export default Navbar;
