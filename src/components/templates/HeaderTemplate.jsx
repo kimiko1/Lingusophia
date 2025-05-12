6a3ee0b99d6db81ce8477e2cdbc2af376cdfdbc3
bastien <bastien.raoult1pro@gmail.com>
2025-05-12 16:38:44 +0200

    ✨(NavBar) Add nav-bar + init project [#1]

---
+++ src/components/templates/HeaderTemplate.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../organisms/Navbar';
+
+const HeaderTemplate = () => {
+  return (
+    <div className="header-template">
+      <Navbar />
+    </div>
+  );
+};
+
+export default HeaderTemplate;
